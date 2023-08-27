107df2e242e8a1079c0a95b1849773da31964508
Ayush Pandey <sjas.ayushpandey101@gmail.com>
2023-08-27 13:21:18 +0530

    fix changed route from dashboard to home

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { indexContoller } = require("../controllers/index");
 
-router.get("/dashboard", indexContoller);
+router.get("/home", indexContoller);
 
 module.exports = router;
