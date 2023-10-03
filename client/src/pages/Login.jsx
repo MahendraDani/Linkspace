55a04acd51d4d3f5ec8c71532c4416a5453cae23
Mahendra Dani <danimahendra0904@gmail.com>
2023-10-03 15:14:20 +0530

    feat: added origin in cors

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/auth/login",
+        "linkspace-api.vercel.app/api/auth/login",
         {
           email: email,
           password: password,
