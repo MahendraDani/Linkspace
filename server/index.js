5392b499c14fb66c8982328ca13ace1d6499787c
Mahendra Dani <danimahendra0904@gmail.com>
2023-10-03 15:16:40 +0530

    feat: removed origin from cors

--- server/index.js
+++ server/index.js
@@ -6,11 +6,7 @@ const { dbConnect } = require("./config/database");
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://linkspace-eta.vercel.app",
-  })
-);
+app.use(cors());
 app.use(bodyPraser.json());
 app.use(bodyPraser.urlencoded({ extended: true }));
 
