0e20f23c907246a2de531c1d31a55fab8dd342d2
Mahendra Dani <danimahendra0904@gmail.com>
2023-11-21 23:00:06 +0530

    test: login route

--- client/src/services/auth/login.service.js
+++ client/src/services/auth/login.service.js
@@ -3,10 +3,9 @@ import axios from "axios";
 export const LoginUser = async (credentials) => {
   let isError = false;
   try {
-    const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/auth/login`,
-      credentials
-    );
+    const url = import.meta.env.VITE_API_URL;
+    console.log(url);
+    const response = await axios.post(`${url}/api/auth/login`, credentials);
     return { isError, response };
   } catch (error) {
     isError = true;
