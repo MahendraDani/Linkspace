5671c144fff34c599bc41d8388913ca1ec2a9bfb
Mahendra Dani <danimahendra0904@gmail.com>
2023-11-22 22:59:31 +0530

    fix: removed all changes

--- client/src/services/auth/login.service.js
+++ client/src/services/auth/login.service.js
@@ -3,9 +3,10 @@ import axios from "axios";
 export const LoginUser = async (credentials) => {
   let isError = false;
   try {
-    const url = import.meta.env.VITE_API_URL;
-    console.log(url);
-    const response = await axios.post(`${url}/api/auth/login`, credentials);
+    const response = await axios.post(
+      `http://localhost:3000/api/auth/login`,
+      credentials
+    );
     return { isError, response };
   } catch (error) {
     isError = true;
