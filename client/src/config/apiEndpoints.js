3146719034454d18325f071c2a30780e76214713
Mahendra Dani <danimahendra0904@gmail.com>
2023-11-23 07:46:26 +0530

    test: added prod url in vercel settings and dev url in env file

--- client/src/config/apiEndpoints.js
+++ client/src/config/apiEndpoints.js
@@ -1,4 +1,6 @@
-export const apiUrl =
-  import.meta.env.VITE_VERCEL_ENV === "production"
-    ? "https://linkspace-api.vercel.app"
-    : "http://localhost:3000";
+// export const apiUrl =
+//   import.meta.env.VITE_VERCEL_ENV === "production"
+//     ? "https://linkspace-api.vercel.app"
+//     : "http://localhost:3000";
+
+export const apiUrl = import.meta.env.VITE_API_URL;
